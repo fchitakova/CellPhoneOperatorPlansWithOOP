4539c78a8a6dd2fc21abeb0c69eb764f15e5d9b3
fchitakova <fchitakova@users.noreply.github.com>
2017-03-12 21:34:34 +0200

    Update Date.h

--- Date.h
+++ Date.h
@@ -10,7 +10,6 @@ public:
 	Date();
 	Date(short d, short m, int y);
 	~Date();
-	Date&operator=(const Date&rhs);
 	void setDay(short d);
 	void setMonth(short m);
 	void setYear(int y);
@@ -19,11 +18,9 @@ public:
 	int getYear()const;
 	void readDate();
 	void printDate()const;
-	bool isValid(const Date&d)const;
 private:
 	short day;
 	short month;
 	int year;
-	void Copy(const Date&rhs);
 };
 #endif
